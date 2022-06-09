1f5edbccfb3e49a1887ff988b780c7efc0738d5d
cahitcanozgenn <cahitcanozgen.dev@gmail.com>
2022-06-09 20:23:29 +0300

    SmartTrashBin

---
+++ SmartTrashBin/SmartTrashBin.ino
@@ -0,0 +1,120 @@
+#include<Servo.h>
+Servo motor;
+#define echo 12
+#define trig 13
+long sure, mesafe;
+  const int motorA1  = 8;  // L298N'in IN3 Girişi
+  const int motorA2  = 9;  // L298N'in IN1 Girişi
+  const int motorB1  = 10; // L298N'in IN2 Girişi
+  const int motorB2  = 11;  // L298N'in IN4 Girişi
+  int i=0; 
+  int j=0; 
+  int state; 
+  int vSpeed=255;     
+
+void setup() {
+    pinMode(motorA1, OUTPUT);
+    pinMode(motorA2, OUTPUT);
+    pinMode(motorB1, OUTPUT);
+    pinMode(motorB2, OUTPUT);    
+    Serial.begin(9600);
+    motor.attach(6);
+    pinMode(trig, OUTPUT);
+    pinMode(echo, INPUT);
+}
+
+
+void loop()
+{
+    if(Serial.available() > 0){     
+      state = Serial.read();   
+    }
+  
+    if (state == '0'){
+      vSpeed=0;}
+    else if (state == '1'){
+      vSpeed=100;}
+    else if (state == '2'){
+      vSpeed=180;}
+    else if (state == '3'){
+      vSpeed=200;}
+    else if (state == '4'){
+      vSpeed=255;}
+     
+   /***********************İleri****************************/
+  //Gelen veri 'F' ise araba ileri gider.
+    if (state == 'F') {
+      analogWrite(motorA1, vSpeed); analogWrite(motorA2, 0);
+        analogWrite(motorB1, vSpeed);      analogWrite(motorB2, 0); 
+    }
+  /**********************İleri Sol************************/
+  //Gelen veri 'G' ise araba ileri sol(çapraz) gider.
+    else if (state == 'G') {
+      analogWrite(motorA1,vSpeed ); analogWrite(motorA2, 0);  
+        analogWrite(motorB1, 100);    analogWrite(motorB2, 0); 
+    }
+  /**********************İleri Sağ************************/
+  //Gelen veri 'I' ise araba ileri sağ(çapraz) gider.
+    else if (state == 'I') {
+        analogWrite(motorA1, 100); analogWrite(motorA2, 0); 
+        analogWrite(motorB1, vSpeed);      analogWrite(motorB2, 0); 
+    }
+  /***********************Geri****************************/
+  //Gelen veri 'B' ise araba geri gider.
+    else if (state == 'B') {
+      analogWrite(motorA1, 0);   analogWrite(motorA2, vSpeed); 
+        analogWrite(motorB1, 0);   analogWrite(motorB2, vSpeed); 
+    }
+  /**********************Geri Sol************************/
+  //Gelen veri 'H' ise araba geri sol(çapraz) gider
+    else if (state == 'H') {
+      analogWrite(motorA1, 0);   analogWrite(motorA2, 100); 
+        analogWrite(motorB1, 0); analogWrite(motorB2, vSpeed); 
+    }
+  /**********************Geri Sağ************************/
+  //Gelen veri 'J' ise araba geri sağ(çapraz) gider
+    else if (state == 'J') {
+      analogWrite(motorA1, 0);   analogWrite(motorA2, vSpeed); 
+        analogWrite(motorB1, 0);   analogWrite(motorB2, 100); 
+    }
+  /***************************Sol*****************************/
+  //Gelen veri 'L' ise araba sola gider.
+    else if (state == 'L') {
+      analogWrite(motorA1, vSpeed);   analogWrite(motorA2, 150); 
+        analogWrite(motorB1, 0); analogWrite(motorB2, 0); 
+    }
+  /***************************Sağ*****************************/
+  //Gelen veri 'R' ise araba sağa gider
+    else if (state == 'R') {
+      analogWrite(motorA1, 0);   analogWrite(motorA2, 0); 
+        analogWrite(motorB1, vSpeed);   analogWrite(motorB2, 150);     
+    }
+  
+  /************************Stop*****************************/
+  //Gelen veri 'S' ise arabayı durdur.
+    else if (state == 'S'){
+        analogWrite(motorA1, 0);  analogWrite(motorA2, 0); 
+        analogWrite(motorB1, 0);  analogWrite(motorB2, 0);
+    }  
+Engel();
+}
+
+void Engel()
+{
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  sure=pulseIn(echo,HIGH);
+  mesafe=sure/58.2;
+  Serial.println(mesafe);
+     if (mesafe <10 )
+     {
+         motor.write(180);
+     }
+     else
+     {
+        motor.write(0);
+     }
+}
